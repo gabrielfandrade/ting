001d68f3cc482a000c8444b03f117c0f133f391e
Gabriel Faustino <gabrielfa.andrade@gmail.com>
2023-03-07 14:38:44 -0300

    feat: .txt Importer

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,18 @@
-def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+import sys
+
+
+def txt_importer(path_file: str):
+    if not path_file.endswith('.txt'):
+        return print('Formato inválido', file=sys.stderr)
+    
+    try:
+        with open(path_file, 'r') as file:
+            content = file.read()
+
+            lines = content.split('\n')
+
+            return lines
+    except FileNotFoundError:
+        return print(f'Arquivo {path_file} não encontrado', file=sys.stderr)
+            
+
