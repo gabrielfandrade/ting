e2ec47401d9ae404ff879a17b70ba934fc8383da
Gabriel Faustino <gabrielfa.andrade@gmail.com>
2023-03-07 14:43:43 -0300

    fix: Lint on txt_importer

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -4,7 +4,7 @@ import sys
 def txt_importer(path_file: str):
     if not path_file.endswith('.txt'):
         return print('Formato inválido', file=sys.stderr)
-    
+
     try:
         with open(path_file, 'r') as file:
             content = file.read()
@@ -14,5 +14,3 @@ def txt_importer(path_file: str):
             return lines
     except FileNotFoundError:
         return print(f'Arquivo {path_file} não encontrado', file=sys.stderr)
-            
-
