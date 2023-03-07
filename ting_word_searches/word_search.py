68cdc57fa15afef78fb7de70a68d1c061384b912
Gabriel Faustino <gabrielfa.andrade@gmail.com>
2023-03-07 16:11:28 -0300

    feat: Exists Word in Queue

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,30 @@
-def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+from ting_file_management.queue import Queue
+
+
+def exists_word(word: str, instance: Queue):
+    results = []
+
+    for file in instance._queue:
+        ocorrencias = []
+
+        for index, line in enumerate(file['linhas_do_arquivo']):
+            if word.casefold() in line.casefold():
+                line_dict = {
+                    'linha': index + 1
+                }
+
+                ocorrencias.append(line_dict)
+
+        if ocorrencias:
+            result = {
+                'palavra': word,
+                'arquivo': file['nome_do_arquivo'],
+                'ocorrencias': ocorrencias
+            }
+
+            results.append(result)
+
+    return results
 
 
 def search_by_word(word, instance):
