d27013a3b44f4fd4091dc3d3136099400ca4f91c
Gabriel Faustino <gabrielfa.andrade@gmail.com>
2023-03-07 15:09:15 -0300

    feat: Enqueue file to Queue

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,20 @@
-def process(path_file, instance):
-    """Aqui irá sua implementação"""
+from ting_file_management.queue import Queue
+from ting_file_management.file_management import txt_importer
+
+
+def process(path_file: str, instance: Queue):
+    if not any(_process['nome_do_arquivo'] == path_file for _process in instance._queue):
+        process_dict = dict()
+
+        lines = txt_importer(path_file)
+
+        process_dict['nome_do_arquivo'] = path_file
+        process_dict['qtd_linhas'] = len(lines)
+        process_dict['linhas_do_arquivo'] = lines
+
+        instance.enqueue(process_dict)
+
+        print(process_dict)
 
 
 def remove(instance):
