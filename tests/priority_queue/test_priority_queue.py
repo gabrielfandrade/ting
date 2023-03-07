2fd9b7297f722ecfbb88015f3b28c4aa3bcfbbec
Gabriel Faustino <gabrielfa.andrade@gmail.com>
2023-03-07 15:48:23 -0300

    test: Priority Queue

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,57 @@
-# from ting_file_management.priority_queue import PriorityQueue
+import pytest
+from ting_file_management.priority_queue import PriorityQueue
 
 
 def test_basic_priority_queueing():
-    """Aqui irá sua implementação"""
+    queue = PriorityQueue()
+
+    files = [
+        {
+            'nome_do_arquivo': 'teste1.txt',
+            'qtd_linhas': 3,
+            'linhas_do_arquivo': [
+                'linha1',
+                'linha2',
+                'linha3',
+            ],
+        },
+        {
+            'nome_do_arquivo': 'teste2.txt',
+            'qtd_linhas': 5,
+            'linhas_do_arquivo': [
+                'linha1',
+                'linha2',
+                'linha3',
+                'linha4',
+                'linha5',
+            ],
+        },
+        {
+            'nome_do_arquivo': 'teste3.txt',
+            'qtd_linhas': 4,
+            'linhas_do_arquivo': [
+                'linha1',
+                'linha2',
+                'linha3',
+                'linha4',
+            ],
+        },
+    ]
+
+    for file in files:
+        queue.enqueue(file)
+
+    assert len(queue) == 3
+    assert len(queue.regular_priority) == 1
+    assert len(queue.high_priority) == 2
+
+    dequeue_file = queue.dequeue()
+
+    assert dequeue_file == files[0]
+
+    search_file = queue.search(1)
+
+    assert search_file == files[1]
+
+    with pytest.raises(IndexError):
+        queue.search(2)
